40fb54aaf229ee2a7611697817d6e8de2553d411
myint <myint@macbookpro.local>
2012-05-06 19:11:52 -0700

    Add acid test logging option

--- test/acid.py
+++ test/acid.py
@@ -5,7 +5,7 @@ Test that autopep8 runs without crashing on various Python files.
 import sys
 
 
-def run(filename, report_incomplete_fix=False, passes=2000):
+def run(filename, log_file, report_incomplete_fix=False, passes=2000):
     """Run autopep8 on file at filename.
     Return True on success.
     """
@@ -21,16 +21,17 @@ def run(filename, report_incomplete_fix=False, passes=2000):
         import tempfile
         with tempfile.NamedTemporaryFile(suffix='.py') as f:
             if 0 != subprocess.call(command, stdout=f):
-                sys.stderr.write('autopep8 crashed on ' + filename + '\n')
+                log_file.write('autopep8 crashed on ' + filename + '\n')
                 return False
 
             if 0 != subprocess.call(['pep8', '--ignore=E501',
-                                     '--show-source', f.name]):
-                sys.stderr.write('autopep8 did not completely fix ' +
-                                 filename + '\n')
+                                     '--show-source', f.name],
+                                    stderr=f):
+                log_file.write('autopep8 did not completely fix ' +
+                               filename + '\n')
     else:
         if 0 != subprocess.call(command + ['--diff']):
-            sys.stderr.write('autopep8 crashed on ' + filename + '\n')
+            log_file.write('autopep8 crashed on ' + filename + '\n')
             return False
 
     return True
@@ -41,18 +42,30 @@ def main():
     parser = optparse.OptionParser()
     parser.add_option('--report-incomplete-fix', action='store_true',
                       help='report incomplete PEP8 fixes')
+    parser.add_option('--log-errors',
+                      help='log autopep8 errors instead of exiting')
     opts, args = parser.parse_args()
 
-    import os
-    for p in sys.path:
-        for root, dirnames, filenames in os.walk(p):
-            import fnmatch
-            for f in fnmatch.filter(filenames, '*.py'):
-                sys.stderr.write('--->  Testing with ' + f + '\n')
+    if opts.log_errors:
+        log_file = open(opts.log_errors, 'w')
+    else:
+        log_file = sys.stderr
 
-                if not run(os.path.join(root, f),
-                           opts.report_incomplete_fix):
-                    sys.exit(1)
+    try:
+        import os
+        for p in sys.path:
+            for root, dirnames, filenames in os.walk(p):
+                import fnmatch
+                for f in fnmatch.filter(filenames, '*.py'):
+                    sys.stderr.write('--->  Testing with ' + f + '\n')
+
+                    if not run(os.path.join(root, f),
+                               log_file=log_file,
+                               report_incomplete_fix=
+                                       opts.report_incomplete_fix):
+                        sys.exit(1)
+    finally:
+        log_file.close()
 
 
 if __name__ == '__main__':
