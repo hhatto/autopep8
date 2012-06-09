e59f0c06585fec3635c5f9453431b4eaff6e716b
myint <myint@macbookpro.local>
2012-06-09 09:50:55 -0700

    Remove import of distutils version of setup()
    There is no point in falling back to this since we make use of
    setuptools features that aren't in distutils. When running against
    distutils, is only partial (without any error message).

--- setup.py
+++ setup.py
@@ -1,10 +1,7 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-try:
-    from setuptools import setup
-except ImportError:
-    from distutils.core import setup
+from setuptools import setup
 import autopep8
 
 # Hack to prevent stupid "TypeError: 'NoneType' object is not callable" error
