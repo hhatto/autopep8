8bc0cd12cf3580c8ebde7d01bedbb8c689a0bd95
myint <myint@macbookpro.local>
2012-06-08 21:23:59 -0700

    Open readme with "with" statement

--- setup.py
+++ setup.py
@@ -16,32 +16,34 @@ try:
 except ImportError:
     pass
 
-setup(
-    name='autopep8',
-    version=autopep8.__version__,
-    description="A tool that automatically formats Python code to conform to "
-                "the PEP 8 style guide",
-    long_description=open("README.rst").read(),
-    license='Expat License',
-    author='Hideo Hattori',
-    author_email='hhatto.jp@gmail.com',
-    url='https://github.com/hhatto/autopep8',
-    classifiers=[
-        'Development Status :: 4 - Beta',
-        'Environment :: Console',
-        'Intended Audience :: Developers',
-        'License :: OSI Approved :: MIT License',
-        'Operating System :: OS Independent',
-        'Programming Language :: Python',
-        'Programming Language :: Python :: 2',
-        'Programming Language :: Python :: 3',
-        'Programming Language :: Unix Shell',
-    ],
-    keywords="automation pep8",
-    install_requires=['pep8'],
-    tests_require=['nose'],
-    test_suite='nose.collector',
-    py_modules=['autopep8'],
-    zip_safe=False,
-    entry_points={'console_scripts': ['autopep8 = autopep8:main']},
-)
+
+with open('README.rst') as readme:
+    setup(
+        name='autopep8',
+        version=autopep8.__version__,
+        description="A tool that automatically formats Python code to conform to "
+                    "the PEP 8 style guide",
+        long_description=readme.read(),
+        license='Expat License',
+        author='Hideo Hattori',
+        author_email='hhatto.jp@gmail.com',
+        url='https://github.com/hhatto/autopep8',
+        classifiers=[
+            'Development Status :: 4 - Beta',
+            'Environment :: Console',
+            'Intended Audience :: Developers',
+            'License :: OSI Approved :: MIT License',
+            'Operating System :: OS Independent',
+            'Programming Language :: Python',
+            'Programming Language :: Python :: 2',
+            'Programming Language :: Python :: 3',
+            'Programming Language :: Unix Shell',
+        ],
+        keywords="automation pep8",
+        install_requires=['pep8'],
+        tests_require=['nose'],
+        test_suite='nose.collector',
+        py_modules=['autopep8'],
+        zip_safe=False,
+        entry_points={'console_scripts': ['autopep8 = autopep8:main']},
+    )
