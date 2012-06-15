5417a90125874ff9c519a04d247556195a36186e
myint <myint@macbookpro.local>
2012-06-14 21:25:14 -0700

    Modify to correctly append results to list
    Previously, it was failing when trying to set a property (result) to be
    a list.

--- autopep8.py
+++ autopep8.py
@@ -25,7 +25,7 @@ import ast
 from distutils.version import StrictVersion
 try:
     import pep8
-    if StrictVersion(pep8.__version__) < StrictVersion('1.3a1'):
+    if StrictVersion(pep8.__version__) < StrictVersion('1.3a2'):
         pep8 = None
 except ImportError:
     pep8 = None
@@ -825,22 +825,34 @@ def _spawn_pep8(pep8_options):
 def _execute_pep8(pep8_options, source, filename=''):
     """Execute pep8 via python method calls."""
     class QuietReport(pep8.BaseReport):
+
         """Version of checker that does not print."""
+
         def __init__(self, options):
             super(QuietReport, self).__init__(options)
-            self.result = []
+            self.__full_error_results = []
 
         def error(self, line_number, offset, text, _):
             """Collect errors."""
             code = super(QuietReport, self).error(line_number, offset, text, _)
             if code:
-                self.result.append(
+                self.__full_error_results.append(
                     dict(id=code, line=line_number,
                          column=offset + 1, info=text))
 
+        def full_error_results(self):
+            """Return error results in detail.
+
+            Results are in the form of a list of dictionaries. Each dictionary
+            contains 'id', 'line', 'column', and 'info'.
+
+            """
+            return self.__full_error_results
+
     checker = pep8.Checker(filename, lines=source,
                            reporter=QuietReport, **pep8_options)
-    return checker.check_all()
+    checker.check_all()
+    return checker.report.full_error_results()
 
 
 class Reindenter(object):
