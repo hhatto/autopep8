5113280e48163dd530a048c408612b237f8bce87
myint <myint@macbookpro.local>
2012-06-22 20:44:17 -0700

    Add missing file close in test setup

--- test/test_autopep8.py
+++ test/test_autopep8.py
@@ -38,12 +38,6 @@ def only_py2(func):
     return func
 
 
-def not_pypy(func):
-    if 'pypy' == sys.subversion[0].lower():
-        func = None
-    return func
-
-
 class TestUtils(unittest.TestCase):
 
     def test_find_newline_only_cr(self):
@@ -1175,10 +1169,9 @@ class TestCoverage(unittest.TestCase):
         os.remove(self.tempfile[1])
 
     def _inner_setup(self, line, options=""):
-        f = open(self.tempfile[1], 'w')
-        f.write(line)
+        with open(self.tempfile[1], 'w') as temp_file:
+            temp_file.write(line)
 
-    @not_pypy
     def test_fixpep8_class_constructor(self):
         line = "print 1\nprint 2\n"
         self._inner_setup(line)
