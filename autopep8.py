697586f65a71b0d0a3964cbe3577ee316df16c0b
myint <myint@macbookpro.local>
2012-06-30 12:33:42 -0700

    Make help message clearer

--- autopep8.py
+++ autopep8.py
@@ -1507,7 +1507,7 @@ def parse_args(args):
                      'used')
 
     if opts.recursive and not (opts.in_place or opts.diff):
-        parser.error('must be used with --in-place or --diff')
+        parser.error('--recursive must be used with --in-place or --diff')
 
     if opts.in_place and opts.diff:
         parser.error('--in-place and --diff are mutually exclusive')
