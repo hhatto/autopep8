cc28bc4f059a993b8b760e0a03e02b3bc821b911
myint <myint@macbookpro.local>
2012-07-28 10:00:49 -0700

    Remove diff

--- update_readme.py
+++ update_readme.py
@@ -10,14 +10,13 @@ except ImportError:
         from io import StringIO
 
 
-def split_readme(readme_path, before_key, after_key, diff_key, end_key):
+def split_readme(readme_path, before_key, after_key, end_key):
     """Return split readme."""
     with open(readme_path) as readme_file:
         readme = readme_file.read()
 
     top, rest = readme.split(before_key)
     before, rest = rest.split(after_key)
-    _, rest = rest.split(diff_key)
     _, bottom = rest.split(end_key)
 
     return (top.rstrip('\n'),
@@ -61,27 +60,14 @@ def indent(text):
     return '\n'.join([indent_line(line) for line in text.split('\n')])
 
 
-def clean_diff(diff_text):
-    """Remove non-deterministic stuff from diff."""
-    clean_lines = []
-    for line in diff_text.split('\n'):
-        if line.startswith('---') or line.startswith('+++'):
-            clean_lines.append(line.split('/')[0])
-        else:
-            clean_lines.append(line)
-    return '\n'.join(clean_lines)
-
-
 def main():
     README_PATH = 'README.rst'
     BEFORE_KEY = 'before::'
     AFTER_KEY = 'after::'
-    DIFF_KEY = 'diff::'
 
     (top, before, bottom) = split_readme(README_PATH,
                                          before_key=BEFORE_KEY,
                                          after_key=AFTER_KEY,
-                                         diff_key=DIFF_KEY,
                                          end_key='options::')
 
     import textwrap
@@ -89,9 +75,6 @@ def main():
         top,
         BEFORE_KEY, before,
         AFTER_KEY, indent(run_autopep8(textwrap.dedent(before))),
-        DIFF_KEY, indent(
-            clean_diff(run_autopep8(textwrap.dedent(before),
-                                    options=['--diff']))),
         bottom])
 
     with open(README_PATH, 'w') as output_file:
