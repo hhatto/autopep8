ce962e9b20347b7e50658ccc667de6cfadf2e895
myint <myint@macbookpro.local>
2012-07-30 22:35:01 -0700

    Flag interesting repositories

--- test/acid_github.py
+++ test/acid_github.py
@@ -1,7 +1,8 @@
 #!/usr/bin/env python
-"""Run acid test against latest packages on Github."""
+"""Run acid test against latest repositories on Github."""
 
 import os
+import re
 import subprocess
 import sys
 
@@ -12,8 +13,8 @@ TMP_DIR = os.path.join(os.path.abspath(os.path.dirname(__file__)),
                        'github_tmp')
 
 
-def latest_packages():
-    """Return names of latest released packages on Github."""
+def latest_repositories():
+    """Return names of latest released repositories on Github."""
     import requests
 
     for result in requests.get('https://github.com/timeline.json').json:
@@ -26,19 +27,25 @@ def latest_packages():
             continue
 
 
-def download_package(name, output_directory):
-    """Download package to output_directory.
+def download_repository(name, output_directory):
+    """Download repository to output_directory.
 
     Raise CalledProcessError on failure.
 
     """
-    original_path = os.getcwd()
-    os.chdir(output_directory)
-    try:
-        subprocess.check_call(
-            ['git', 'clone', name])
-    finally:
-        os.chdir(original_path)
+    subprocess.check_call(['git', 'clone', name],
+                          cwd=output_directory)
+
+
+def interesting(repository_path):
+    """Return True if interesting."""
+    print(repository_path)
+    process = subprocess.Popen(['git', 'log'],
+                               cwd=repository_path,
+                               stdout=subprocess.PIPE)
+    return len(re.findall(
+        'pep8',
+        process.communicate()[0].decode('utf-8'))) > 2
 
 
 def main():
@@ -58,8 +65,9 @@ def main():
     import time
     start_time = time.time()
 
-    checked_packages = []
-    skipped_packages = []
+    checked_repositories = []
+    skipped_repositories = []
+    interesting_repositories = []
     try:
         while True:
             if opts.timeout > 0 and time.time() - start_time > opts.timeout:
@@ -70,52 +78,61 @@ def main():
                     break
             else:
                 while not names:
-                    # Continually populate if user did not specify a package
+                    # Continually populate if user did not specify a repository
                     # explicitly.
-                    names = [p for p in latest_packages()
-                             if p not in checked_packages and
-                             p not in skipped_packages]
+                    names = [p for p in latest_repositories()
+                             if p not in checked_repositories and
+                             p not in skipped_repositories]
 
                     if not names:
                         import time
                         time.sleep(1)
 
-            package_name = names.pop(0)
-            print(package_name)
+            repository_name = names.pop(0)
+            print(repository_name)
 
             user_tmp_dir = os.path.join(
                 TMP_DIR,
-                os.path.basename(os.path.split(package_name)[0]))
+                os.path.basename(os.path.split(repository_name)[0]))
             try:
                 os.mkdir(user_tmp_dir)
             except OSError:
                 pass
 
-            package_tmp_dir = os.path.join(
+            repository_tmp_dir = os.path.join(
                 user_tmp_dir,
-                os.path.basename(package_name))
+                os.path.basename(repository_name))
             try:
-                os.mkdir(package_tmp_dir)
+                os.mkdir(repository_tmp_dir)
             except OSError:
-                print('Skipping already checked package')
-                skipped_packages.append(package_name)
+                print('Skipping already checked repository')
+                skipped_repositories.append(repository_name)
                 continue
 
             try:
-                download_package(package_name, output_directory=package_tmp_dir)
+                download_repository(repository_name,
+                                    output_directory=repository_tmp_dir)
             except subprocess.CalledProcessError:
                 print('ERROR: git clone failed')
                 continue
 
-            if acid.check(opts, [package_tmp_dir]):
-                checked_packages.append(package_name)
+            if acid.check(opts, [repository_tmp_dir]):
+                checked_repositories.append(repository_name)
+
+                if interesting(
+                    os.path.join(repository_tmp_dir,
+                                 os.path.basename(repository_name))):
+                    interesting_repositories.append(repository_name)
             else:
                 return 1
     except KeyboardInterrupt:
         pass
 
-    if checked_packages:
-        print('\nTested packages:\n    ' + '\n    '.join(checked_packages))
+    if checked_repositories:
+        print('\nTested repositories:')
+        for name in checked_repositories:
+            print('    ' + name +
+                  (' *' if name in interesting_repositories else ''))
 
 if __name__ == '__main__':
     sys.exit(main())
