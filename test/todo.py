929a9b9f3e0e0c9769e3b44609bfec9f9cc79b87
myint <myint@macbookpro.local>
2012-09-21 19:43:31 -0700

    Correct typo

--- test/todo.py
+++ test/todo.py
@@ -1,6 +1,6 @@
 """Incomplete fixes."""
 
 
-# Make sure of logical line to shorten long physical lines.
+# Make use of logical line parsing to shorten long physical lines.
 one_two_three_four_five_six = {'one two three four five': 12345, 'asdfsdflsdkfjl sdflkjsdkfkjsfjsdlkfj sdlkfjlsfjs': '343',
                                1: 1}
