a9a7b7f84cb0050418c7ed9dec0f2162d94fb647
myint <myint@macbookpro.local>
2012-11-03 14:11:41 -0700

    Remove too early return

--- autopep8.py
+++ autopep8.py
@@ -462,10 +462,12 @@ class FixPEP8(object):
         original = self.source[line_index]
         fixed = original
 
-        if '(' in logical_lines[0]:
-            fixed = logical_lines[0].find('(') * ' ' + original.lstrip()
-        elif '[' in logical_lines[0]:
-            fixed = logical_lines[0].find('[') * ' ' + original.lstrip()
+        for symbol in ['(', '[', '{']:
+            if symbol in logical_lines[0]:
+                fixed = logical_lines[0].find(symbol) * ' ' + original.lstrip()
+
+        if fixed != original:
+            pass
         elif logical_lines[0].rstrip().endswith('\\'):
             fixed = (_get_indentation(logical_lines[0]) +
                      self.indent_word + original.lstrip())
