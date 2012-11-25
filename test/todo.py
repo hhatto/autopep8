620295638cf062d315726f93d694d51edabe01ec
myint <macbookpro.local>
2012-11-24 19:13:09 -0800

    Remove completed item

--- test/todo.py
+++ test/todo.py
@@ -4,7 +4,3 @@
 # Make use of logical line parsing to shorten long physical lines.
 one_two_three_four_five_six = {'one two three four five': 12345, 'asdfsdflsdkfjl sdflkjsdkfkjsfjsdlkfj sdlkfjlsfjs': '343',
                                1: 1}
-
-
-# Code with vertical tabs
-# (https://github.com/MartinMelo/SistemasOperativos/blob/master/Model/Recursos/CPU/CPU.py).
