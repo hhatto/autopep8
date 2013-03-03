13d3fd51fb401a714384b1d35de39b9b7515cacf
Steven Myint <git@stevenmyint.com>
2013-03-03 13:25:52 -0800

    Add missing names/dates to copyright notice

--- autopep8.py
+++ autopep8.py
@@ -1,5 +1,8 @@
 #!/usr/bin/env python
 #
+# Copyright (C) 2010-2011 Hideo Hattori
+# Copyright (C) 2011-2013 Hideo Hattori, Steven Myint
+#
 # Permission is hereby granted, free of charge, to any person obtaining
 # a copy of this software and associated documentation files (the
 # "Software"), to deal in the Software without restriction, including
