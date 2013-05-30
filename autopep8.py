5c2a11c139000106d0d034bf7be22713c553b92b
Steven Myint <git@stevenmyint.com>
2013-05-29 20:15:31 -0700

    Remove case already handled by pep8

--- autopep8.py
+++ autopep8.py
@@ -134,11 +134,7 @@ def extended_blank_lines(logical_line,
         if blank_lines and pep8.DOCSTRING_REGEX.match(logical_line):
             yield (0, 'E303 too many blank lines ({0})'.format(blank_lines))
     elif (pep8.DOCSTRING_REGEX.match(previous_logical) and indent_level):
-        if (blank_lines and
-                not logical_line.startswith(('def ', 'class ', '@'))):
-            yield (0, 'E303 too many blank lines ({0})'.format(blank_lines))
-        elif (not blank_lines and
-              logical_line.startswith(('def ', 'class ', '@'))):
+        if (not blank_lines and logical_line.startswith(('def ', '@'))):
             yield (0, 'E301 expected 1 blank line, found 0')
 pep8.register_check(extended_blank_lines)
 
