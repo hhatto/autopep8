822b94600286178ff49ae8a168ada3008354b457
Steven Myint <git@stevenmyint.com>
2013-07-04 16:24:28 -0700

    Check test code with pylint too
    Use more lenient settings.

--- test/test_autopep8.py
+++ test/test_autopep8.py
@@ -616,7 +616,7 @@ sys.maxint
         self.assertEqual(
             """\
 -foo
-\ No newline at end of file
+\\ No newline at end of file
 +foo
 """,
             '\n'.join(autopep8.get_diff_text(['foo'],
