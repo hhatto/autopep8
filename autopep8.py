c3c01d003cacf6135e784a1b568ea471169bf12f
Steven Myint <git@stevenmyint.com>
2013-07-11 21:48:52 -0700

    Add the only remaining relevant future import

--- autopep8.py
+++ autopep8.py
@@ -34,6 +34,7 @@ documentation for more information.
 
 """
 
+from __future__ import absolute_import
 from __future__ import division
 from __future__ import print_function
 from __future__ import unicode_literals
