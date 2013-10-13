04c9b30fcec5ac11322a02091d9d7632ee4e89d5
Steven Myint <git@stevenmyint.com>
2013-10-13 15:33:43 -0700

    Avoid putting inline comment on the next line

--- autopep8.py
+++ autopep8.py
@@ -1150,7 +1150,6 @@ def _shorten_line(tokens, source, indentation, indent_word, newline,
         token_string = tkn[1]
 
         if (
-            aggressive and
             token_type == tokenize.COMMENT and
             not previous_line.rstrip().endswith('\\') and
             not source[tkn[2][1] + 1:].strip().lower().startswith(
