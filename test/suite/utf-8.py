997aec17d42ffd34c43b0981f27547950dbf5223
Steven Myint <git@stevenmyint.com>
2013-12-09 06:29:30 -0800

    Reindent
    pep8 alone did not detect this.

--- test/suite/utf-8.py
+++ test/suite/utf-8.py
@@ -3,18 +3,18 @@
 
 class Rectangle(Blob):
 
-        def __init__(self, width, height,
-                     color='black', emphasis=None, highlight=0):
-            if width == 0 and height == 0 and \
-                    color == 'red' and emphasis == 'strong' or \
-                    highlight > 100:
-                raise ValueError("sorry, you lose")
-            if width == 0 and height == 0 and (color == 'red' or
-                                               emphasis is None):
-                raise ValueError("I don't think so -- values are %s, %s" %
-                                 (width, height))
-            Blob.__init__(self, width, height,
-                          color, emphasis, highlight)
+    def __init__(self, width, height,
+                 color='black', emphasis=None, highlight=0):
+        if width == 0 and height == 0 and \
+                color == 'red' and emphasis == 'strong' or \
+                highlight > 100:
+            raise ValueError("sorry, you lose")
+        if width == 0 and height == 0 and (color == 'red' or
+                                           emphasis is None):
+            raise ValueError("I don't think so -- values are %s, %s" %
+                             (width, height))
+        Blob.__init__(self, width, height,
+                      color, emphasis, highlight)
 
 
 # Some random text with multi-byte characters (utf-8 encoded)
