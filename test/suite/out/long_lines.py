ee9c5c8265b4971a9b593d252711a88f59fe6b75
Steven Myint <git@stevenmyint.com>
2013-12-21 18:44:22 -0800

    Update due to correction to E501 usage

--- test/suite/out/long_lines.py
+++ test/suite/out/long_lines.py
@@ -2,7 +2,8 @@ if True:
     if True:
         if True:
             self.__heap.sort(
-                )  # pylint: builtin sort probably faster than O(n)-time heapify
+            )  # pylint: builtin sort probably faster than O(n)-time heapify
 
             if True:
-                foo = '(                                                ' + array[0] + '                    '
+                foo = '(                                                ' + \
+                    array[0] + '                    '
