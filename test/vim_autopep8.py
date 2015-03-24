5436068e2a0974a932d59d51dd529af221832735
Steven Myint <git@stevenmyint.com>
2015-03-23 19:26:17 -0700

    Support Python 3 in Vim usage example

--- test/vim_autopep8.py
+++ test/vim_autopep8.py
@@ -2,12 +2,36 @@
 
 map <C-I> :pyfile <path_to>/vim_autopep8.py<CR>
 
+Replace ":pyfile" with ":py3file" if Vim is built with Python 3 support.
+
 """
 
+from __future__ import unicode_literals
+
+import sys
+
 import vim
+
+
+ENCODING = vim.eval('&fileencoding')
+
+
+def encode(text):
+    if sys.version_info[0] >= 3:
+        return text
+    else:
+        return text.encode(ENCODING)
+
+
+def decode(text):
+    if sys.version_info[0] >= 3:
+        return text
+    else:
+        return text.decode(ENCODING)
+
+
 if vim.eval('&syntax') == 'python':
-    encoding = vim.eval('&fileencoding')
-    source = '\n'.join(line.decode(encoding)
+    source = '\n'.join(decode(line)
                        for line in vim.current.buffer) + '\n'
 
     import autopep8
@@ -22,5 +46,5 @@ if vim.eval('&syntax') == 'python':
         if formatted.endswith('\n'):
             formatted = formatted[:-1]
 
-        vim.current.buffer[:] = [line.encode(encoding)
+        vim.current.buffer[:] = [encode(line)
                                  for line in formatted.splitlines()]
