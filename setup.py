bb3f8d4cfd563e7d7234d8c365ea55c439b7f928
Steven Myint <git@stevenmyint.com>
2015-09-19 08:17:08 -0700

    Test on Python 3.5

--- setup.py
+++ setup.py
@@ -49,6 +49,7 @@ with io.open('README.rst') as readme:
             'Programming Language :: Python :: 3.2',
             'Programming Language :: Python :: 3.3',
             'Programming Language :: Python :: 3.4',
+            'Programming Language :: Python :: 3.5',
             'Topic :: Software Development :: Libraries :: Python Modules',
             'Topic :: Software Development :: Quality Assurance',
         ],
