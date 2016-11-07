c0f4dd983842b60edb10de0bfa889bf41f58af9b
⁣ Florian Best <spaceone@users.noreply.github.com>
2016-11-07 04:39:43 +0100

    E301 has been further split into E305, E306
    Adjust to API changes in pycodestyle 2.1; Issue #274

--- autopep8.py
+++ autopep8.py
@@ -456,6 +456,8 @@ class FixPEP8(object):
         self.fix_e273 = self.fix_e271
         self.fix_e274 = self.fix_e271
         self.fix_e309 = self.fix_e301
+        self.fix_e305 = self.fix_e301
+        self.fix_e306 = self.fix_e301
         self.fix_e501 = (
             self.fix_long_line_logically if
             options and (options.aggressive >= 2 or options.experimental) else
