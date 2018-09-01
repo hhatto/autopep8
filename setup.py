e469a153967efb521cb8f831c315f246b25efa2b
Hideo Hattori <hhatto.jp@gmail.com>
2018-09-02 01:01:26 +0900

    py37

--- setup.py
+++ setup.py
@@ -46,6 +46,7 @@ with io.open('README.rst') as readme:
             'Programming Language :: Python :: 3.4',
             'Programming Language :: Python :: 3.5',
             'Programming Language :: Python :: 3.6',
+            'Programming Language :: Python :: 3.7',
             'Topic :: Software Development :: Libraries :: Python Modules',
             'Topic :: Software Development :: Quality Assurance',
         ],
