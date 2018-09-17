26e116ba8187f4b7aa5dc68457d620fa3df2d55c
ConleyLee <lika52090@gmail.com>
2018-09-17 21:48:51 +0800

    support python2

--- test/test_gb2312_stdin.py
+++ test/test_gb2312_stdin.py
@@ -1,14 +1,19 @@
 import os
-
+import platform
+import re
 
 def main():
+    os_name  = platform.platform()
+    if not re.match('windows', os_name, flags=re.IGNORECASE):
+        return
+
     dir_path = os.path.dirname(os.path.abspath(__file__))
-    path = dir_path + '\\suite\\windows-gb2312.txt'
+    path = os.path.join(dir_path, 'suite', 'windows-gb2312.txt')
     print('test old version....')
     os.system('type %s | autopep8 -' % path)
 
     print('test new version')
-    autopep8_path = dir_path + '\\..\\autopep8.py'
+    autopep8_path = os.path.join(dir_path, '..', 'autopep8.py')
     os.system('type %s | python %s -' % (path, autopep8_path))
 
 
