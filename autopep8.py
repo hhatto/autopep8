1ad18df567d411e6ef1182d55a1be2530406f8b3
Peter Law <PeterJCLaw@gmail.com>
2022-09-16 18:41:05 +0100

    Rename E266 fixer to have the right name
    It still ends up actually fixing E265 as well (it always did both)
    however that one is going to be easier to reproduce as a standard
    fixer.

--- autopep8.py
+++ autopep8.py
@@ -1685,7 +1685,7 @@ def split_and_strip_non_empty_lines(text):
     return [line.strip() for line in text.splitlines() if line.strip()]
 
 
-def fix_e265(source, aggressive=False):  # pylint: disable=unused-argument
+def fix_e266(source, aggressive=False):  # pylint: disable=unused-argument
     """Format block comments."""
     if '#' not in source:
         # Optimization.
